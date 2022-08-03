d2e324fdfa325a193537d1221ecd080b4f5efd07
armenium <armen@digidez.com>
2022-08-03 02:44:43 +0200

    MysteryBox functionality (4)

--- app/Http/Controllers/MysteryBoxController.php
+++ app/Http/Controllers/MysteryBoxController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Variant;
 
 class MysteryBoxController extends Controller {
 
+	public function getVintageHandpickItem(){
+		$query = Variant::query();
+		$query->select('variants.*');
+		$query->leftJoin('products', 'variants.product_id', '=', 'products.product_id');
+		$query->leftJoin('tags', 'tags.product_id', '=', 'products.product_id');
+		$query->where([
+			'products.is_mystery' => 0,
+			'products.status' => 'active',
+			'products.title' => "NOT LIKE %REWORK%",
+			'tags.tag' => 'GG',
+			'variants.inventory_quantity' => 1,
+		]);
+		#$query->where('products.title', 'NOT LIKE ');
+		
+		$data = $query->get();
+	}
 
 }
